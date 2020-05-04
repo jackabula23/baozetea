337679f9400e689a0651386089309fa6e41021c6
jackabula23 <jackabula23@gmail.com>
2020-05-04 21:51:59 +0800

    origin/baozetea

--- resources/views/layouts/index.blade.php
+++ resources/views/layouts/index.blade.php
@@ -127,12 +127,12 @@ License URL: http://creativecommons.org/licenses/by/3.0/
 												<div class="multi-gd-img">
 													<ul class="multi-column-dropdown">
 														<h6>All Groceries</h6>
-														<li><a href="groceries.html">Dals & Pulses</a></li>
-														<li><a href="groceries.html">Almonds</a></li>
-														<li><a href="groceries.html">Cashews</a></li>
-														<li><a href="groceries.html">Dry Fruit</a></li>
-														<li><a href="groceries.html"> Mukhwas </a></li>
-														<li><a href="groceries.html">Rice & Rice Products</a></li>
+														<li><a href="#">Dals & Pulses</a></li>
+														<li><a href="#">Almonds</a></li>
+														<li><a href="#">Cashews</a></li>
+														<li><a href="#">Dry Fruit</a></li>
+														<li><a href="#"> Mukhwas </a></li>
+														<li><a href="#">Rice & Rice Products</a></li>
 													</ul>
 												</div>													
 											</div>
@@ -145,12 +145,12 @@ License URL: http://creativecommons.org/licenses/by/3.0/
 												<div class="multi-gd-img">
 													<ul class="multi-column-dropdown">
 														<h6>All Household</h6>
-														<li><a href="household.html">Cookware</a></li>
-														<li><a href="household.html">Dust Pans</a></li>
-														<li><a href="household.html">Scrubbers</a></li>
-														<li><a href="household.html">Dust Cloth</a></li>
-														<li><a href="household.html"> Mops </a></li>
-														<li><a href="household.html">Kitchenware</a></li>
+														<li><a href="#">Cookware</a></li>
+														<li><a href="#">Dust Pans</a></li>
+														<li><a href="#">Scrubbers</a></li>
+														<li><a href="#">Dust Cloth</a></li>
+														<li><a href="#"> Mops </a></li>
+														<li><a href="#">Kitchenware</a></li>
 													</ul>
 												</div>																								
 											</div>
@@ -163,12 +163,12 @@ License URL: http://creativecommons.org/licenses/by/3.0/
 												<div class="multi-gd-img">
 													<ul class="multi-column-dropdown">
 														<h6>Baby Care</h6>
-														<li><a href="personalcare.html">Baby Soap</a></li>
-														<li><a href="personalcare.html">Baby Care Accessories</a></li>
-														<li><a href="personalcare.html">Baby Oil & Shampoos</a></li>
-														<li><a href="personalcare.html">Baby Creams & Lotion</a></li>
-														<li><a href="personalcare.html"> Baby Powder</a></li>
-														<li><a href="personalcare.html">Diapers & Wipes</a></li>
+														<li><a href="#">Baby Soap</a></li>
+														<li><a href="#">Baby Care Accessories</a></li>
+														<li><a href="#">Baby Oil & Shampoos</a></li>
+														<li><a href="#">Baby Creams & Lotion</a></li>
+														<li><a href="#"> Baby Powder</a></li>
+														<li><a href="#">Diapers & Wipes</a></li>
 													</ul>
 												</div>												
 											</div>
@@ -181,12 +181,12 @@ License URL: http://creativecommons.org/licenses/by/3.0/
 												<div class="multi-gd-img">
 													<ul class="multi-column-dropdown">
 														<h6>All Accessories</h6>
-														<li><a href="packagedfoods.html">Baby Food</a></li>
-														<li><a href="packagedfoods.html">Dessert Items</a></li>
-														<li><a href="packagedfoods.html">Biscuits</a></li>
-														<li><a href="packagedfoods.html">Breakfast Cereals</a></li>
-														<li><a href="packagedfoods.html"> Canned Food </a></li>
-														<li><a href="packagedfoods.html">Chocolates & Sweets</a></li>
+														<li><a href="#">Baby Food</a></li>
+														<li><a href="#">Dessert Items</a></li>
+														<li><a href="#">Biscuits</a></li>
+														<li><a href="#">Breakfast Cereals</a></li>
+														<li><a href="#"> Canned Food </a></li>
+														<li><a href="#">Chocolates & Sweets</a></li>
 													</ul>
 												</div>											
 											</div>
@@ -199,20 +199,20 @@ License URL: http://creativecommons.org/licenses/by/3.0/
 												<div class="multi-gd-img">
 													<ul class="multi-column-dropdown">
 														<h6>Tea & Coeffe</h6>
-														<li><a href="beverages.html">Green Tea</a></li>
-														<li><a href="beverages.html">Ground Coffee</a></li>
-														<li><a href="beverages.html">Herbal Tea</a></li>
-														<li><a href="beverages.html">Instant Coffee</a></li>
-														<li><a href="beverages.html"> Tea </a></li>
-														<li><a href="beverages.html">Tea Bags</a></li>
+														<li><a href="#">Green Tea</a></li>
+														<li><a href="#">Ground Coffee</a></li>
+														<li><a href="#">Herbal Tea</a></li>
+														<li><a href="#">Instant Coffee</a></li>
+														<li><a href="#"> Tea </a></li>
+														<li><a href="#">Tea Bags</a></li>
 													</ul>
 												</div>
 							
 											</div>
 										</ul>
 									</li>
-									<li><a href="gourmet.html">Gourmet</a></li>
-									<li><a href="offers.html">Offers</a></li>
+									<li><a href="#">Gourmet</a></li>
+									<li><a href="#">Offers</a></li>
 									<li><a href="{{url('contact')}}">Contact</a></li>
 								</ul>
 							</div>
@@ -246,11 +246,11 @@ License URL: http://creativecommons.org/licenses/by/3.0/
 				<div class="col-md-3 w3_footer_grid">
 					<h3>產品分類</h3>
 					<ul class="info"> 
-						<li><i class="fa fa-arrow-right" aria-hidden="true"></i><a href="groceries.html">Groceries</a></li>
-						<li><i class="fa fa-arrow-right" aria-hidden="true"></i><a href="household.html">Household</a></li>
-						<li><i class="fa fa-arrow-right" aria-hidden="true"></i><a href="personalcare.html">Personal Care</a></li>
-						<li><i class="fa fa-arrow-right" aria-hidden="true"></i><a href="packagedfoods.html">Packaged Foods</a></li>
-						<li><i class="fa fa-arrow-right" aria-hidden="true"></i><a href="beverages.html">Beverages</a></li>
+						<li><i class="fa fa-arrow-right" aria-hidden="true"></i><a href="#">Groceries</a></li>
+						<li><i class="fa fa-arrow-right" aria-hidden="true"></i><a href="#">Household</a></li>
+						<li><i class="fa fa-arrow-right" aria-hidden="true"></i><a href="#">Personal Care</a></li>
+						<li><i class="fa fa-arrow-right" aria-hidden="true"></i><a href="#">Packaged Foods</a></li>
+						<li><i class="fa fa-arrow-right" aria-hidden="true"></i><a href="#">Beverages</a></li>
 					</ul>
 				</div>
 				<div class="col-md-3 w3_footer_grid">
